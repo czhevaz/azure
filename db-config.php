58f9fc1cac9736a042d9129fef9dc80a1811e7a2
Asev Setiawan <asevsetiawan@gmail.com>
2019-05-01 16:21:08 +0700

    Create db-config.php

---
+++ db-config.php
@@ -0,0 +1,12 @@
+<?php
+$serverName = "dicodingazureapp.database.windows.net"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"dicodingazureDB", "UID"=>"asev", "PWD"=>"One2345678");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
